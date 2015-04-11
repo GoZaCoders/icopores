50c36d1a211debbcca506fbf5bf5deb25b458833
GoZaCoders <gozacoders@gmail.com>
2015-04-10 22:18:27 -0500

    Revert "Instalacion"
    This reverts commit 1ab2d269123b248ad2c21d3b15f74e6e8004cb48.

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '0854398372d73ef91a763d153baf34ef';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
